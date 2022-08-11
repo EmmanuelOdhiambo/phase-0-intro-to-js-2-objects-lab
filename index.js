62005fd2484161e6584c18d2dd91d2249cce4000
Emmanuel Odhiambo <emmanuel3odhiambo@gmail.com>
2022-08-12 00:11:11 +0300

    intro to Jsobjects lab complete

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+const employee = {
+    name: "Cooper", 
+    streetAddress: "10th Mfangano Street"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+const newEmployee = {...employee};
+  newEmployee[key] = value;
+
+  return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key, value) {
+    return Object.assign({}, employee, { [key]: value });
+}
+
+function destructivelyDeleteFromEmployeeByKey(
+    employee, key, value) {
+        delete employee[key];
+        return employee;
+    };
+
